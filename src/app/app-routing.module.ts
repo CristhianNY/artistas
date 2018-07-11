6597027ee4e1bffdb1fff659e9ec529e23d9de1e
cristhian Bonilla <cristhian4545@gmail.com>
2018-07-10 20:57:40 -0500

    se agregan rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomePageComponent} from './components/home-page/home-page.component';
+import {LoginPageComponent} from './components/login-page/login-page.component';
+import {RegisterPageComponent} from './components/register-page/register-page.component';
+import {DashboardPageComponent} from './components/dashboard-page/dashboard-page.component';
+import {NotFoundPageComponent} from './components/not-found-page/not-found-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',component: HomePageComponent},
+  {path: 'login', component: LoginPageComponent},
+  {path: 'registrarse', component: RegisterPageComponent},
+  {path: 'admin', component:DashboardPageComponent},
+  {path: '**', component: NotFoundPageComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
