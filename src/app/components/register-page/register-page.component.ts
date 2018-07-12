9a260b5ae600ee7766715ffe2400e0ae3fc7e1b8
cristhian Bonilla <cristhian4545@gmail.com>
2018-07-11 22:19:42 -0500

    se crea registro temporal con firebase

--- src/app/components/register-page/register-page.component.ts
+++ src/app/components/register-page/register-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AuthService} from '../../services/auth.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register-page',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-page.component.css']
 })
 export class RegisterPageComponent implements OnInit {
-
-  constructor() { }
+  public email:string;
+  public password:string;
+  constructor(
+    public authServices: AuthService,
+    public router: Router) { 
+   
+  }
 
   ngOnInit() {
   }
 
+  onSubmitAddUser(){
+    this.authServices.registerUser(this.email,this.password)
+    .then((res)=>{
+     this.router.navigate(['/admin']);
+    }).catch( (err) =>{
+      console.log("Error");
+      
+    });
+  }
 }
